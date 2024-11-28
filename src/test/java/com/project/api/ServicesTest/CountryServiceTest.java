5f7f335217c38c6efad0f764fcf49ef78f1c50ca
IvanM2077 <123nba789@gmail.com>
2024-11-28 09:28:14 -0600

    refactor the layer of repositories, adding the layer or dataprovider and initialite the services test

---
+++ src/test/java/com/project/api/ServicesTest/CountryServiceTest.java
@@ -0,0 +1,2 @@
+package com.project.api.ServicesTest;public class CountryServiceTest {
+}
---
+++ src/test/resources/Querys/db.sql
@@ -0,0 +1,2 @@
+INSERT INTO address (address_id, address_line, city) VALUES (1, '123 Main St', 'City A');
+INSERT INTO address (address_id, address_line, city) VALUES (2, '456 Elm St', 'City B');
